539a10763687b10c941beea52c8767d7ecda9a5c
Alex <kuzminbass@gmail.com>
2021-10-26 09:20:27 +1000

    new revert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates :email, :presence => true, :uniqueness => true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  validates :username ,:email, :presence => true, :uniqueness => true
   has_secure_password
   has_many :lessons
   has_many :bookings
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
     # get "/lessons/new"
     # get 'lessons/new'
 
